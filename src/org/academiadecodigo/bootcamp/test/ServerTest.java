fe07cb66351b093cf16614715adebc8a9fc99a30
jesseakers82 <jesseakers82@gmail.com>
2017-03-03 17:47:15 +0000

    updates

--- src/org/academiadecodigo/bootcamp/test/ServerTest.java
+++ src/org/academiadecodigo/bootcamp/test/ServerTest.java
@@ -6,6 +6,8 @@ import org.academiadecodigo.bootcamp.client.Company;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -160,6 +162,12 @@ public class ServerTest implements Runnable {
 
 
     }
+
+    Cadet cadet = new Cadet();
+
+    List<Cadet> companyYes = new LinkedList<>();
+
+
 }
 
 
